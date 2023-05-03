0e41d8c75545be8033347cf20f113900562aa62b
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-05-04 01:18:57 +0300

    Extend tailwindcss utilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+      gridTemplateRows: {
+        "layout-3": "auto 1fr auto",
+        "layout-4": "auto auto 1fr auto",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
