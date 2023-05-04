ffce7a34487cd701a74f471f76ca8afc955a5282
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-05-04 17:14:02 +0300

    Add ProtectedPage utility component

---
+++ src/components/ProtectedPage/ProtectedPage.tsx
@@ -0,0 +1,29 @@
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import React from "react";
+
+type ProtectedPageProps = {
+  children: React.ReactNode;
+};
+
+// Component to handle private page
+const PrivatePage = ({ children }: ProtectedPageProps) => {
+  return (
+    <>
+      <SignedIn>
+        {/*
+          Authenticated visitors will see the page content.
+        */}
+        {children}
+      </SignedIn>
+      <SignedOut>
+        {/* 
+          Non-authenticated visitors will be redirected
+          to the sign in page.
+        */}
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  );
+};
+
+export default PrivatePage;
