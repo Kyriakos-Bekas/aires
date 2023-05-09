65ad2d0d2b4f229409b5235a5528d87b04649cf1
Alkis Pouros <alkispou35@gmail.com>
2023-05-09 18:25:02 +0300

    Made some changes in layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,9 @@ const Home: NextPage = () => {
         <meta name="description" content="AirES" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container flex items-center">
-        <div className="grow text-center">
+      <main className="container grid grid-cols-2 items-center">
+        <div className="grid grid-rows-2 items-center grow text-center h-full w-full">
+            <h1 className="font-semibold text-6xl p-2 m-4"><a className="text-red-600">Extreme</a> Sports <i><a className="text-red-600">extreme</a>ly</i> easy!</h1>
           <h2 className="text-xl">
             <SignedIn>
               Hello <span className="font-semibold">{user?.fullName}</span>!
@@ -26,6 +27,10 @@ const Home: NextPage = () => {
               Hello <span className="font-semibold">Guest</span>!
             </SignedOut>
           </h2>
+            
+          </div>
+          <div className="overflow-hidden max-h-full">
+            <img src="https://images.unsplash.com/photo-1551524559-8af4e6624178?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80" alt="Ski jump photo" className="h-full w-full object-cover rounded-xl"/>
         </div>
       </main>
     </BasicLayout>
