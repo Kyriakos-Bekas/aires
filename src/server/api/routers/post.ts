4046dc4f9b28228a1ef7d428ddcdeadba03cfaa2
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-05-11 23:30:32 +0300

    Add getOne query to fetch specific post by id

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -10,4 +11,33 @@ export const postRouter = createTRPCRouter({
 
     return posts.map(({ published, ...returnedPost }) => returnedPost);
   }),
+  getOne: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+      if (!input.id) return null; // TODO: Handle this better
+
+      const post = await ctx.prisma.post.findUnique({
+        where: { id: input.id },
+      });
+
+      // If the post doesn't exist, throw a NOT_FOUND error
+      if (!post) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Post not found",
+        });
+      }
+
+      // If the post isn't published, throw an UNAUTHORIZED error
+      if (!post.published) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "You cannot access this post",
+        });
+      }
+
+      const { published, ...returnedPost } = post;
+
+      return returnedPost;
+    }),
 });
