9b5e5dca35771ede21ad4fa81646f2168b457cc7
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-05-16 01:36:51 +0300

    Add Redirect component
    Redirects users to the pages that should be visible to their role

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Navigation } from "./Navigation";
-export { default as ProtectedPage } from "./ProtectedPage";
+export * from "./ProtectedPage";
 export { default as Footer } from "./Footer";
 export { default as Post } from "./Post";
 export { default as Button } from "./Button";
