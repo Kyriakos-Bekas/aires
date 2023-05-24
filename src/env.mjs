33bac58e4b2428acb9519d55f9482ce47f1b5ac5
Alkis Pouros <alkispou35@gmail.com>
2023-05-24 17:09:33 +0300

    Removed duplicate environment variable declaration from merge

--- src/env.mjs
+++ src/env.mjs
@@ -11,7 +11,7 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "test", "production"]),
     AIRLABS_APIKEY: z.string().min(1),
     RAPIDAPI_APIKEY: z.string().min(1),
-    AIRLABS_APIKEY: z.string().min(1),
+    
   },
 
   /**
@@ -32,7 +32,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     AIRLABS_APIKEY: process.env.AIRLABS_APIKEY,
     RAPIDAPI_APIKEY: process.env.RAPIDAPI_APIKEY,
-    AIRLABS_APIKEY: process.env.AIRLABS_APIKEY,
+    
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
 });
