31760108c2b9104ce02ef7e4ddcdd637a12ceacd
Evangelia Kontemenioti <evangelia.kont01@outlook.com>
2023-06-08 19:35:57 +0300

    About tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,47 +4,45 @@ import Head from "next/head";
 import { BasicLayout } from "~/layouts";
 import { api } from "~/utils/api";
 
-const Home: NextPage = () => {
+const About: NextPage = () => {
     const { user } = useUser();
   
     return (
-      <BasicLayout>
         <head>
-        <title>About Us | AirES</title>
-    </head>
+            <title>About Us | AirES</title>
+        </head>
 
-    <body>
+        <body>
 
-    <nav>
-        <a href="https://aires.vercel.app/">AirES</a>
-        <a href="https://aires.vercel.app/">Home</a>
-        <a href="https://aires.vercel.app/events/">Events</a>
-        <a href="https://aires.vercel.app/about/">About</a>
-    </nav>
+        <nav>
+            <a href="https://aires.vercel.app/">AirES</a>
+            <a href="https://aires.vercel.app/">Home</a>
+            <a href="https://aires.vercel.app/events/">Events</a>
+            <a href="https://aires.vercel.app/about/">About</a>
+        </nav>
 
-    <div class="message">
-        <p>Since 2023, we are bringing your desire for adventure, fast travel planning and comfortable experiences into reality.</p>
-    </div>
-
-    <div class="details">
-        <p>In this day and age when the desire for getaways and out-of-the-world experiences has increased more than ever, a trip planner for extreme sport events is the ideal solution. At AirES, we are bringing together the most popular airlines, accommodations and extreme sport venues to organize your most ideal trip with all of your favorite destinations and activities.</p>
-    </div>
-
-    <div class="photo">
-        <img src="https://sportsfacilities.com/wp-content/uploads/2016/12/shutterstock_482102515-1024x350-1.jpg">
-    </div>
-
-    <div class="row">
-        <div class="column">
-            <h1></h1>
+        <div class="message">
+            <p>Since 2023, we are bringing your desire for adventure, fast travel planning and comfortable experiences into reality.</p>
         </div>
-        <div class="column">
-            <h1></h1>
+
+        <div class="details">
+            <p>In this day and age when the desire for getaways and out-of-the-world experiences has increased more than ever, a trip planner for extreme sport events is the ideal solution. At AirES, we are bringing together the most popular airlines, accommodations and extreme sport venues to organize your most ideal trip with all of your favorite destinations and activities.</p>
         </div>
-    </div>
-    </body>
-      </BasicLayout>
+
+        <div class="photo">
+            <img src="https://sportsfacilities.com/wp-content/uploads/2016/12/shutterstock_482102515-1024x350-1.jpg">
+        </div>
+
+        <div class="row">
+            <div class="column">
+                <h1></h1>
+            </div>
+            <div class="column">
+                <h1></h1>
+            </div>
+        </div>
+        </body>
     );
   };
   
-  export default Home;
+  export default About;
