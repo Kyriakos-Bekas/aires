1c510b4199fbefd55a8f4946c103aed0669cec6a
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-12 15:09:26 +0300

    Add mapCodeToLocation utility function

--- src/utils/mapCodeToLocation.ts
+++ src/utils/mapCodeToLocation.ts
@@ -1,7 +1,7 @@
 import { locations, type LocationCode } from "~/data/locations";
 
 export default function mapCodeToLocation(code: LocationCode) {
-  const location = locations.find((location) => location.code === code);
+  const location = locations.find((location) => location.city_code === code);
   if (!location) {
     throw new Error(`Location with code ${code} not found`);
   }
