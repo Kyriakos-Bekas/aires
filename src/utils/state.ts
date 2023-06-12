094450ea7c6e55da2e8d068dd5bd49456c8bc2db
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-12 15:10:40 +0300

    Add state for plan tracking

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -13,3 +13,29 @@ export const useUserStore = create<UserStore>((set) => ({
   setId: () => set((state) => ({ id: state.id })),
   setIsPartner: () => set((state) => ({ isPartner: state.isPartner })),
 }));
+
+type PlanStore = {
+  id: string | null;
+  toFlight: string | null;
+  accommodation: string | null;
+  fromFlight: string | null;
+  eventId: string;
+  setId: (id: string | null) => void;
+  setToFlight: (toFlight: string | null) => void;
+  setAccommodation: (accommodation: string | null) => void;
+  setFromFlight: (fromFlight: string | null) => void;
+  setEventId: (eventId: string) => void;
+};
+
+export const usePlanStore = create<PlanStore>((set) => ({
+  id: null,
+  toFlight: null,
+  accommodation: null,
+  fromFlight: null,
+  eventId: "",
+  setId: (id) => set({ id }),
+  setToFlight: (toFlight) => set({ toFlight }),
+  setAccommodation: (accommodation) => set({ accommodation }),
+  setFromFlight: (fromFlight) => set({ fromFlight }),
+  setEventId: (eventId) => set({ eventId }),
+}));
